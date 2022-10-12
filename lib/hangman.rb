0e73b53d74f6b82b61c7b751fdb13b96425ce640
jacksoze <kamonyojack@gmail.com>
2022-10-12 16:38:41 +0300

    Add a letter check method for game play

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,45 +1,68 @@
-
-File.open('words.txt') #the words are pasted from google-10000-english-no-swears.txt dictionary file from the first20hours GitHub repository google-10000-english
+File.open('words.txt') # the words are pasted from google-10000-english-no-swears.txt dictionary file from the first20hours GitHub repository google-10000-english
 
 class Game
-
   def initialize
-   game_play
+
+    @guess = 5
+    @word = ''
+    game_play
   end
 
+  attr_accessor :letter, :guess, :word
+
   def secret_word_selection
-    word = ""
-    until word.length > 5 && word.length < 12
+
     word = File.readlines('words.txt').sample.delete_suffix('\n')
-    end
+    puts word
     word
   end
 
-  def game_play
-    #display dashes according to the word.length
-    word = secret_word_selection
-    split_word = word.split('')
-
-    puts word
-    array = []
-    (word.length-1).times{array.push('_')}
-    dashes = array.join(' ')
-
-
-    #ask the player to input a letter
+  def player_letter_input
     puts 'Please input a letter between a->z'
-    letter = gets.chomp.downcase
-    #if the word.include(letter), replace the dash in letter position
-    if split_word.include?(letter)
-      array[split_word.index(letter)] = letter
-    end
-    puts array
-    puts array.join(' ')
-
-    #if not, subtract the guess by 1 and post the letter to an array
-    #repeat until word is complete or gusses end
+    self.letter = gets.chomp.downcase
   end
 
+  def letter_check
 
+
+    wrong_letters = []
+    selected_word_arr = @word.split('')
+    dashes_arr = []
+
+    (word.length - 1).times { dashes_arr.push('_') }
+
+    dashes = dashes_arr.join(' ')
+    puts dashes
+    until guess == 0
+      self.guess -= 1
+      if selected_word_arr.include?(letter)
+
+        selected_word_arr.each_with_index do |lettr, index|
+          dashes_arr[index] = letter if lettr == letter
+        end
+        puts dashes_arr.join(' ')
+        player_letter_input
+      else
+        wrong_letters.push(letter)
+        puts "Incorrect letters: #{wrong_letters}"
+        letter = gets.chomp.downcase
+
+      end
+    end
+  end
+
+  def game_play
+    # display dashes according to the word.length
+    self.word = secret_word_selection
+
+
+
+    # ask the player to input a letter
+    player_letter_input
+    letter_check
+  end
+
+  # if not, subtract the guess by 1 and post the letter to an dashes_arr
+  # repeat until word is complete or gusses end
 end
 game = Game.new
